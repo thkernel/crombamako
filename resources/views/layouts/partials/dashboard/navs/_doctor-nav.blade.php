fb9e94bf72ea32a94766205576fcaa08171b3136
thkernel <a.dembele@upcase.net>
2021-02-01 15:28:13 +0000

    update

--- resources/views/layouts/partials/dashboard/navs/_doctor-nav.blade.php
+++ resources/views/layouts/partials/dashboard/navs/_doctor-nav.blade.php
@@ -11,7 +11,50 @@
 
   
   
+
+  <li class="br-menu-item">
+    <a href="#" class="br-menu-link with-sub ">
+      <i class="fa fa-building-o tx-20"></i>
+      <span class="menu-item-label">Structures</span>
+    </a><!-- br-menu-link -->
+    <ul class="br-menu-sub">
+      <li class="sub-item">
+        <a class="sub-link" href="{{ route('structures.index') }}">Structures</a>
+
+      </li>
+
+      <li class="sub-item">
+        <a class="sub-link" href="{{ route('staffs.index') }}">Personnel</a>
+
+      </li>
+
+     
+      <li class="sub-item">
+        <a class="sub-link" href="{{ route('business_licenses.index') }}">Licences d'exploitation</a>
+
+      </li>
+
+      <li class="sub-item">
+        <a class="sub-link" href="{{ route('visit_summaries.index') }}">Résumés de visite</a>
+
+      </li>
+
+    </ul>
+  </li>
+
+
   
+  
+
+  <li class="br-menu-item">
+    
+
+    <a class="br-menu-link" href="{{ route('approvals.index') }}">
+      <i class="fa fa-id-card-o tx-20" aria-hidden="true"></i> <span class="menu-item-label">Agrément</span>
+    
+    </a>
+
+  </li><!-- br-menu-item -->
 
   <li class="br-menu-item">
     
@@ -26,41 +69,25 @@
   <li class="br-menu-item">
     
 
-    <a class="br-menu-link" href="{{ route('certificate_requests.index') }}">
+    <a class="br-menu-link" href="{{ route('document_requests.index') }}">
       <i class="fa fa-file-text-o tx-20" aria-hidden="true"></i> <span class="menu-item-label">Demandes de document</span>
     
     </a>
 
   </li><!-- br-menu-item -->
 
+
+
   
-  
 
+    
 
-  <li class="br-menu-item">
-          <a href="#" class="br-menu-link with-sub ">
-            <i class="fa fa-cubes tx-20"></i>
-            <span class="menu-item-label">Opportunités</span>
-          </a><!-- br-menu-link -->
-          <ul class="br-menu-sub">
-            
+    
 
-            <li class="sub-item">
-              <a class="sub-link" href="{{ route('opportunities.index') }}">Opportunités</a>
-
-            </li>
-
-          </ul>
-        </li>
-
-   
-   
-
-       
         
         
 
-      
+        
 
        
       </ul><!-- br-sideleft-menu -->
