eb1fc169af13c5f4c778989dd02b29b270aa30ab
thkernel <a.dembele@upcase.net>
2021-03-18 15:51:06 +0000

    update

--- resources/views/structure_services/index.blade.php
+++ resources/views/structure_services/index.blade.php
@@ -15,10 +15,10 @@
         </div>
 
         <div class="text-right">
-          @can('read', App\Models\StructureService::class) 
+          
 
             <a class="btn btn-primary tx-mont tx-medium tx-11 tx-uppercase pd-y-12 pd-x-25 tx-spacing-1" href="{{ route('structure_services.create') }}"><i class="fa fa-plus" aria-hidden="true"></i>Ajouter</a>
-          @endcan
+          
 
         </div>
 
