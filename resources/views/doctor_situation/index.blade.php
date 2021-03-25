36eb272ced9a4a80e9f03c71bff0ca10ecb97597
thkernel <a.dembele@upcase.net>
2021-03-25 09:01:44 +0000

    update

--- resources/views/doctor_situation/index.blade.php
+++ resources/views/doctor_situation/index.blade.php
@@ -9,51 +9,53 @@
 		</div>
 	</div>
 
-		@if ($results == null || $results->count() <= 0)
-		    <div class="container main-container">
-		        <div class="row">
-		            <div class="col-md-12">
-		                <div class="card">
-		                    <div class="card-body text-center">
-		                        <p>
-		                            <h5>Pas de résultat pour cette recherche!</h5>
-		                        </p>
-		                    </div>
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		@else
+	@if ($results == null || $results->count() <= 0)
+	    <div class="container main-container">
+	        <div class="row">
+	            <div class="col-md-12">
+	                <div class="card">
+	                    <div class="card-body text-center">
+	                        <p>
+	                            <h5>Pas de résultat pour cette recherche!</h5>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	@else
 		    
-			<div class="container main-container">
-        		<div class="card">
-            		<div class="card-body">
-                		<div class="row">
+		<div class="container main-container">
+        	<div class="card">
+            	<div class="card-body">
+                	<div class="row">
 
 
-		    <table id="datatable" class="table display responsive nowrap">
+				    	<table id="datatable" class="table display responsive nowrap">
 
 
-			  <thead>
-			    <tr>
-			      <th>Date</th>
-			      <th>N°Inscrip.</th>
-			      <th>Médecin</th>
-			    
-			    </tr>
-			  </thead>
+						  <thead>
+						    <tr>
+						      <th>Date</th>
+						      <th>N°Inscrip.</th>
+						      <th>Médecin</th>
+						    
+						    </tr>
+						  </thead>
 
-			  <tbody class="doctors">
-			    @include('doctor_situation/partials/_search_result')
-			  </tbody>
-			</table>
+						  <tbody class="doctors">
+						    @include('doctor_situation/partials/_search_result')
+						  </tbody>
+						</table>
+					</div>
+				</div>
+                
+            </div>
+        </div>
+    </div>
 
 
 		    
-
-		    
-		</div>
-	</div>
 
 
 			
--- resources/views/doctor_situation/partials/_search_result.blade.php
+++ resources/views/doctor_situation/partials/_search_result.blade.php
@@ -1,56 +1,9 @@
 
 
 
-    <div class="container main-container">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
 
 
-                    
-                    @foreach($results as $structure)
-                        <div class="col-md-3">
-                            <div class="card">
-
-                                <div class="structure-logo">
-                                    <a href="{{ route('show_structure_path', $structure->slug)}}">
-
-                                      {!!  structure_logo($structure, "card-img-top", "") !!}
-                                    </a>
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">
-                                      <a href="{{ route('show_structure_path', $structure->slug)}}">
-
-                                          {{ $structure->name }}
-                                      </a>
-                                    </h5>
-                                    <p class="card-text">
-                                      {{ $structure->address}} {{ $structure->town->name }}
-                                    </p>
-
-                                    <p class="card-text">
-                                      
-                                    </p>
-
-                                    <a href="{{ route('show_structure_path', $structure->slug)}}" class="btn btn-primary btn-block">
-
-                                          Fiche
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="text-center">
-                    
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-@endif
+@foreach($results as $structure)
+    
+@endforeach
+   
